92f217d3c62120490743a7814fb458256b7f2d94
qqbbgisme <irenecheung.hortung@gmail.com>
2025-10-01 22:29:04 +0800

    update image

--- src/config.ts
+++ src/config.ts
@@ -20,61 +20,39 @@ export const siteConfig = {
       description:
         "I participated in the HKUST Technical Proposal, and my team won the first place. We proposed a solution to help the HKUST to improve their student management system.",
       link: "https://qqbbgisme.github.io/PwC_Internship_Journey/projects/hkust",
-      skills: ["Deck Making"],
+      skills: ["Slide Design Mastery", "Global HR Insights", "Legal Terminology", "Research Skills"],
     },
     {
       name: "EdUHK Technical Proposal",
       description:
         "Given prior experience in higher education, I was given the opportunity in building a technical proposal for the EdUHK",
       link: "https://fullstackextensions.com/?ref=devportfolio",
-      skills: ["React", "Node.js", "AWS"],
+      skills: ["Tender Preparation", "Vendor Coordination" ],
     },
     {
       name: "BBPOS SAP SuccessFactors",
       description:
         "Kit to jump-start your Chrome extension projects with a variety of battle-tested starter templates & examples",
       link: "https://extensionkit.io/?ref=devportfolio",
-      skills: ["SAP SuccessFactors Configuration"],
+      skills: ["SAP SuccessFactors Configuration", "On Site Support", "UAT Testing", "User Manual Creation"],
     },
   ],
-  experience: [
+    experience: [
     {
-      company: "HSBC Limited",
-      title: "Technology Student Intern - Payme Engineering",
-      dateRange: "Jun 2025 - Aug 2025",
+      company: "SAP Team",
+      title: "The most caring and supportive team!!! ",
       bullets: [
-        "Developed an AI-powered chatbot utilizing Retrieval-Augmented Generation (RAG) framework to generate Splunk Query Language (SPL) queries, supporting advanced use cases including information mapping, journey performance trend analysis, user activity monitoring, and troubleshooting recommendations.",
        "Designed and implemented backend and frontend systems, integrating API gateway, RESTful APIs, vector database management, and optimized data preprocessing to ensure scalable, maintainable, and high-performance functionality.",
        "Delivered an end-to-end AI chatbot project, managing development, testing, and deployment phases while adhering to high, standards of code quality and maintainability across the project lifecycle.",
       ],
     },
     {
-      company: "PwC Hong Kong",
-      title: "Technology Consulting Intern",
-      dateRange: "Feb 2025 - Jun 2025",
+      company: "Batchmates^^",
+      title: "The most hustling and alcholic batchmates!!!",
       bullets: [
         "Supported implementation of a cloud-based Human Resources Information System (SAP SuccessFactors), focusing on system configuration, data migration, and optimization of HR workflows through business process reengineering and harmonization.",
         "Collaborated cross-functionally with stakeholders to track deliverables, prepare progress reports, and coordinate meetings, ensuring seamless integration and alignment with project requirements.",
         "Conducted research and analysis to identify opportunities for process improvements, facilitating data-driven decisions in HR system design and maintenance. Implemented CI/CD pipeline reducing deployment time by 60% Collaborated with product team to define technical requirements"
-      ],
-    },
-    {
-      company: "Hermès Asia Pacific Limited",
-      title: "Commercial Intern",
-      dateRange: "Sep 2024 - Feb 2025",
-      bullets: [
-        "Presented data analytics insights on APAC sales and inventory data trends to aid credit decisions and B2B expansion strategies.",
-        "Optimized inventory management while maintaining product assortment mix over 70%; managed product and market research, benchmarking, and sales data analysis to identify competitive advantages and formulate strategies for product launches."
-      ],
-    },
-    {
-      company: "Cartier, Richemont Asia Pacific Limited",
-      title: "Retail Operations Intern",
-      dateRange: "Jun 2024 - Aug 2024",
-      bullets: [
-        "Performed post-event evaluations to measure project effectiveness and identify areas for operational improvement.",
-        "Supported retail performance tracking via key metrics analysis and reporting, enabling data-driven decisions and improvements.",
-        "Led the “Art of Serving” project to establish a new service standard by developing strategies to elevate customer experience."
       ],
     },
   ],
